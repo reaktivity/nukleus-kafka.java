ac64c0d0ed2d3738b7dc9442e252dadbd774e08b
Chris Barrow <chris.barrow@kaazing.com>
2017-12-14 19:58:14 -0800

    Added method to calculate the hash code for a fetch key using code adapted from the Kafka client, plus tests showing it produces the same results.

---
+++ src/main/java/org/reaktivity/nukleus/kafka/internal/util/BufferUtil.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright 2016-2017 The Reaktivity Project
+ *
+ * The Reaktivity Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.reaktivity.nukleus.kafka.internal.util;
+
+import org.agrona.DirectBuffer;
+
+public final class BufferUtil
+{
+
+    /**
+     * Adapted from
+     * https://apache.googlesource.com/kafka/+/trunk/clients/src/main/java/org/apache/kafka/common/utils/Utils.java
+     *
+     * Generates 32 bit murmur2 hash from a range of bytes
+     * @param data buffer to hash from position offset (inclusive) to limit (exclusive)
+     * @return 32 bit hash of the given array
+     */
+    public static int murmur2(
+        final DirectBuffer buffer,
+        final int offset,
+        final int limit)
+    {
+        final int length = limit - offset;
+        int seed = 0x9747b28c;
+        // 'm' and 'r' are mixing constants generated offline.
+        // They're not really 'magic', they just happen to work well.
+        final int m = 0x5bd1e995;
+        final int r = 24;
+        // Initialize the hash to a random value
+        int h = seed ^ length;
+        int length4 = length / 4;
+        for (int i = 0; i < length4; i++)
+        {
+            final int i4 = offset + i * 4;
+            int k = (buffer.getByte(i4 + 0) & 0xff) + ((buffer.getByte(i4 + 1) & 0xff) << 8) +
+                    ((buffer.getByte(i4 + 2) & 0xff) << 16) + ((buffer.getByte(i4 + 3) & 0xff) << 24);
+            k *= m;
+            k ^= k >>> r;
+            k *= m;
+            h *= m;
+            h ^= k;
+        }
+        // Handle the last few bytes of the input array
+        final int remaining = length - 4 * length4;
+        if (remaining == 3)
+        {
+            h ^= (buffer.getByte(offset + (length & ~3) + 2) & 0xff) << 16;
+        }
+        if (remaining >= 2)
+        {
+            h ^= (buffer.getByte(offset + (length & ~3) + 1) & 0xff) << 8;
+        }
+        if (remaining >= 1)
+        {
+            h ^= buffer.getByte(offset + (length & ~3)) & 0xff;
+            h *= m;
+        }
+
+        h ^= h >>> 13;
+        h *= m;
+        h ^= h >>> 15;
+        return h;
+    }
+
+}
---
+++ src/test/java/org/reaktivity/nukleus/kafka/internal/util/BufferUtilTest.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright 2016-2017 The Reaktivity Project
+ *
+ * The Reaktivity Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.reaktivity.nukleus.kafka.internal.util;
+
+import static java.nio.ByteBuffer.allocateDirect;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.assertEquals;
+
+import org.agrona.MutableDirectBuffer;
+import org.agrona.concurrent.UnsafeBuffer;
+import org.apache.kafka.common.utils.Utils;
+import org.junit.Test;
+
+public final class BufferUtilTest
+{
+
+    private final MutableDirectBuffer buffer = new UnsafeBuffer(allocateDirect(100))
+    {
+        {
+            // Make sure the code is not secretly relying upon memory being initialized to 0
+            setMemory(0, capacity(), (byte) 0xab);
+        }
+    };
+
+    @Test
+    public void shouldGenerateHashForOneByteValue()
+    {
+        final int offset = 11;
+        byte[] data = "a".getBytes(UTF_8);
+        buffer.putBytes(offset, data);
+        assertEquals(Utils.murmur2(data), BufferUtil.murmur2(buffer, offset, offset + data.length));
+    }
+
+    @Test
+    public void shouldGenerateHashForTwoByteValue()
+    {
+        final int offset = 45;
+        byte[] data = "ab".getBytes(UTF_8);
+        buffer.putBytes(offset, data);
+        assertEquals(Utils.murmur2(data), BufferUtil.murmur2(buffer, offset, offset + data.length));
+    }
+
+    @Test
+    public void shouldGenerateHashForThreeByteValue()
+    {
+        final int offset = 67;
+        byte[] data = "abc".getBytes(UTF_8);
+        buffer.putBytes(offset, data);
+        assertEquals(Utils.murmur2(data), BufferUtil.murmur2(buffer, offset, offset + data.length));
+    }
+
+    @Test
+    public void shouldGenerateHashForFourByteValue()
+    {
+        final int offset = 23;
+        byte[] data = "abcd".getBytes(UTF_8);
+        buffer.putBytes(offset, data);
+        assertEquals(Utils.murmur2(data), BufferUtil.murmur2(buffer, offset, offset + data.length));
+    }
+
+    @Test
+    public void shouldGenerateHashFor40ByteValue()
+    {
+        final int offset = 38;
+        byte[] data = "abcdefghij1234567890abcdefghij1234567890".getBytes(UTF_8);
+        buffer.putBytes(offset, data);
+        assertEquals(Utils.murmur2(data), BufferUtil.murmur2(buffer, offset, offset + data.length));
+    }
+
+    @Test
+    public void shouldGenerateHashFor41ByteValue()
+    {
+        final int offset = 38;
+        byte[] data = "abcdefghij1234567890abcdefghij1234567890a".getBytes(UTF_8);
+        buffer.putBytes(offset, data);
+        assertEquals(Utils.murmur2(data), BufferUtil.murmur2(buffer, offset, offset + data.length));
+    }
+
+    @Test
+    public void shouldGenerateHashFor42ByteValue()
+    {
+        final int offset = 38;
+        byte[] data = "abcdefghij1234567890abcdefghij1234567890ab".getBytes(UTF_8);
+        buffer.putBytes(offset, data);
+        assertEquals(Utils.murmur2(data), BufferUtil.murmur2(buffer, offset, offset + data.length));
+    }
+
+    @Test
+    public void shouldGenerateHashFor43ByteValue()
+    {
+        final int offset = 38;
+        byte[] data = "abcdefghij1234567890abcdefghij1234567890abc".getBytes(UTF_8);
+        buffer.putBytes(offset, data);
+        assertEquals(Utils.murmur2(data), BufferUtil.murmur2(buffer, offset, offset + data.length));
+    }
+}
