45cdbae43acc97b4f84d1161052a048b92b7abec
John Fallows <john.fallows@kaazing.com>
2018-03-30 16:11:28 -0700

    Update dependency versions

--- src/test/java/org/reaktivity/nukleus/kafka/internal/control/ControllerIT.java
+++ src/test/java/org/reaktivity/nukleus/kafka/internal/control/ControllerIT.java
@@ -45,7 +45,7 @@ public class ControllerIT
         .commandBufferCapacity(1024)
         .responseBufferCapacity(1024)
         .counterValuesBufferCapacity(1024)
-        .controller(KafkaController.class::isAssignableFrom);
+        .controller("kafka"::equals);
 
     @Rule
     public final TestRule chain = outerRule(k3po).around(timeout).around(controller);
