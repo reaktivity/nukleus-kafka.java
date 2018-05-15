3a0429b68c47446439af920c8d26aed929e98def
Chris Barrow <chris.barrow@kaazing.com>
2018-05-14 18:36:10 -0700

    More work on PartitionCache, especially to handle out of order messages being added.

--- src/main/java/org/reaktivity/nukleus/kafka/internal/cache/PartitionCache.java
+++ src/main/java/org/reaktivity/nukleus/kafka/internal/cache/PartitionCache.java
@@ -23,31 +23,48 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import org.agrona.DirectBuffer;
+import org.agrona.collections.LongArrayList;
 import org.agrona.concurrent.UnsafeBuffer;
 import org.reaktivity.nukleus.kafka.internal.stream.HeadersFW;
+import org.reaktivity.nukleus.kafka.internal.types.OctetsFW;
 
 public class PartitionCache
 {
-    private static final int NO_MESSAGE = -1;
+    public static final int TOMBSTONE_MESSAGE = -2;
+
+    private static final int NO_MESSAGE = MessageCache.NO_MESSAGE;
     private static final int NO_POSITION = -1;
+    private static final long NO_EXPIRY_TIME = -1L;
 
     private final MessageCache messageCache;
-    private final EntryIterator iterator = new EntryIterator();
-    private final UnsafeBuffer buffer = new UnsafeBuffer(EMPTY_BYTE_ARRAY);
-
+    private final long tombstoneLifetimeMillis;
     private final Map<UnsafeBuffer, Entry> offsetsByKey;
     private final List<Entry> entries;
-    private int compactFrom = NO_POSITION;
+
+    private final List<DirectBuffer> tombstoneKeys = new ArrayList<>(100);
+    private final LongArrayList tombstoneExpiryTimes = new LongArrayList(100, NO_EXPIRY_TIME);
+
+    private final EntryIterator iterator = new EntryIterator();
+    private final NoMessagesIterator noMessagesIterator = new NoMessagesIterator();
+    private final UnsafeBuffer buffer = new UnsafeBuffer(EMPTY_BYTE_ARRAY);
+    private final Entry candidate = new Entry(0L, NO_MESSAGE, NO_POSITION);
+
+    private int compactFrom = Integer.MAX_VALUE;
+    private long validFromOffset = 0L;
+    private long validToOffset = 0L;
 
     public PartitionCache(
         int initialCapacity,
+        int tombstoneLifetimeMillis,
         MessageCache messageCache)
     {
         this.offsetsByKey = new HashMap<>(initialCapacity);
         entries = new ArrayList<Entry>(initialCapacity);
         this.messageCache = messageCache;
+        this.tombstoneLifetimeMillis = tombstoneLifetimeMillis;
     }
 
     public void add(
@@ -60,41 +77,47 @@ public class PartitionCache
         DirectBuffer value)
     {
         long messageOffset = nextFetchOffset - 1;
-        long highestOffset = entries.isEmpty() ? -1L : entries.get(entries.size() - 1).offset;
         Entry entry = null;
 
-        if (messageOffset > highestOffset)
+        // Only cache if there are no gaps in observed offsets and we have not yet observed this offset
+        if (requestOffset <= validToOffset && messageOffset >= validToOffset)
         {
+            validToOffset = Math.max(validToOffset,  nextFetchOffset);
             buffer.wrap(key, 0, key.capacity());
-            if (value == null)
+            entry = offsetsByKey.get(buffer);
+            if (entry == null)
             {
-                entry = offsetsByKey.remove(buffer);
-                messageCache.release(entry.message);
-                compactFrom = compactFrom == NO_POSITION ? entry.position : compactFrom;
-                entry.position = NO_POSITION;
-            }
-            else
-            {
-                entry = offsetsByKey.get(buffer);
-                if (entry == null)
+                UnsafeBuffer keyCopy = new UnsafeBuffer(new byte[key.capacity()]);
+                keyCopy.putBytes(0,  key, 0, key.capacity());
+                entry = new Entry(messageOffset, NO_MESSAGE, entries.size());
+                offsetsByKey.put(keyCopy, entry);
+                entries.add(entry);
+                if (value == null)
                 {
-                    UnsafeBuffer keyCopy = new UnsafeBuffer(new byte[key.capacity()]);
-                    keyCopy.putBytes(0,  key, 0, key.capacity());
-                    entry = new Entry(messageOffset, entries.size(), NO_MESSAGE);
-                    offsetsByKey.put(keyCopy, entry);
-                    entries.add(entry);
-                    entry.message = messageCache.put(timestamp, traceId, key, headers, value);
+                    entry.message = TOMBSTONE_MESSAGE;
+                    tombstoneKeys.add(key);
+                    tombstoneExpiryTimes.add(System.currentTimeMillis() + tombstoneLifetimeMillis);
                 }
                 else
                 {
-                    if (messageOffset > entry.offset)
-                    {
-                        compactFrom = compactFrom == NO_POSITION ? entry.position : compactFrom;
-                        entry.position = entries.size();
-                        entry.offset = messageOffset;
-                        entries.add(entry);
-                        entry.message = messageCache.replace(entry.message, timestamp, traceId, key, headers, value);
-                    }
+                    entry.message = messageCache.put(timestamp, traceId, key, headers, value);
+                }
+            }
+            else
+            {
+                compactFrom = Math.min(compactFrom, entry.position);
+                entry.position = entries.size();
+                entry.offset = messageOffset;
+                entries.add(entry);
+                if (value == null)
+                {
+                    entry.message = TOMBSTONE_MESSAGE;
+                    tombstoneKeys.add(key);
+                    tombstoneExpiryTimes.add(System.currentTimeMillis() + tombstoneLifetimeMillis);
+                }
+                else
+                {
+                    entry.message = messageCache.replace(entry.message, timestamp, traceId, key, headers, value);
                 }
             }
         }
@@ -103,18 +126,57 @@ public class PartitionCache
             // cache the message if it was previously evicted due to lack of space
             buffer.wrap(key, 0, key.capacity());
             entry = offsetsByKey.get(buffer);
-            if (entry.message == NO_MESSAGE)
+            if (entry != null && entry.message == NO_MESSAGE)
             {
-                // TODO: only cache if the message was of interest to at least one dispatcher
-                //       (call next dispatcher)
+                // TODO: only cache the message if it was of was of interest to at least one dispatcher
                 entry.message = messageCache.put(timestamp, traceId, key, headers, value);
             }
         }
     }
 
-    public void compact()
+    public Iterator<Entry> entries(
+        long requestOffset)
     {
-        if (compactFrom != NO_POSITION)
+        Iterator<Entry> result;
+        int position = locate(requestOffset);
+        if (position == -1)
+        {
+            long offset = requestOffset < validFromOffset ? requestOffset : Math.max(requestOffset, validToOffset);
+            result = noMessagesIterator.reset(offset);
+        }
+        else
+        {
+            iterator.position = position;
+            result = iterator;
+        }
+        return result;
+    }
+
+    public Entry getEntry(
+        long requestOffset,
+        OctetsFW key)
+    {
+        buffer.wrap(key.buffer(), key.offset(), key.sizeof());
+        Entry result = offsetsByKey.get(buffer);
+        if (result == null)
+        {
+            long offset = requestOffset < validFromOffset ? requestOffset : validToOffset;
+            result = noMessagesIterator.reset(offset).next();
+        }
+        return result;
+    }
+
+    public void setPartitionStartOffset(
+        long firstOffset)
+    {
+        assert validFromOffset == 0L && validToOffset == 0L;
+        validFromOffset = validToOffset = firstOffset;
+    }
+
+    private void compact()
+    {
+        evictExpiredTombstones();
+        if (compactFrom < entries.size())
         {
             int ceiling = NO_POSITION;
             for (int i=0; i < entries.size(); i++)
@@ -141,24 +203,60 @@ public class PartitionCache
                     entries.remove(i);
                 }
             }
-            compactFrom = NO_POSITION;
+            compactFrom = Integer.MAX_VALUE;
         }
     }
 
-    public Iterator<Entry> entries(
-        long startOffset)
+    private void evictExpiredTombstones()
     {
-        iterator.position = locate(startOffset);
-        return iterator;
+        if (tombstoneKeys.size() > 0)
+        {
+            final long now = System.currentTimeMillis();
+            int pos = 0;
+            for (pos=0; pos < tombstoneKeys.size(); pos++)
+            {
+                if (now >= tombstoneExpiryTimes.getLong(pos))
+                {
+                    tombstoneExpiryTimes.setLong(pos, NO_EXPIRY_TIME);
+                    DirectBuffer key = tombstoneKeys.set(pos,  null);
+                    buffer.wrap(key, 0, key.capacity());
+                    Entry entry = offsetsByKey.remove(buffer);
+                    compactFrom = Math.min(entry.position, compactFrom);
+                    entry.position = NO_POSITION;
+                }
+                else
+                {
+                    // expiry times are in increasing order
+                    break;
+                }
+            }
+            if (pos > 0)
+            {
+                tombstoneKeys.removeIf(k -> k == null);
+                tombstoneExpiryTimes.removeIf(t -> t == null);
+            }
+        }
     }
 
     private int locate(
         long offset)
     {
         compact();
-        Entry candidate = new Entry(offset, NO_POSITION, NO_MESSAGE);
-        int position = Collections.binarySearch(entries, candidate);
-        return Math.max(0, position);
+        candidate.offset = offset;
+        int result;
+        if (offset >= validToOffset)
+        {
+            result = NO_POSITION;
+        }
+        else
+        {
+            result = Collections.binarySearch(entries, candidate);
+            if (result < 0)
+            {
+                result = -result - 1;
+            }
+        }
+        return result;
     }
 
     public final class EntryIterator implements Iterator<Entry>
@@ -178,21 +276,53 @@ public class PartitionCache
         }
     }
 
+    public final class NoMessagesIterator implements Iterator<Entry>
+    {
+        private Entry entry = new Entry(0L, NO_MESSAGE, NO_POSITION);
+        private int remaining;
+
+        NoMessagesIterator reset(long offset)
+        {
+            entry.offset = offset;
+            remaining = 1;
+            return this;
+        }
+
+        @Override
+        public boolean hasNext()
+        {
+            return remaining > 0;
+        }
+
+        @Override
+        public Entry next()
+        {
+            if (remaining-- > 0)
+            {
+                return entry;
+            }
+            else
+            {
+                 throw new NoSuchElementException();
+            }
+        }
+    }
+
     public static final class Entry implements Comparable<Entry>
     {
         private long offset;
-        private int  position;
         private int  message;
+        private int  position;
 
         private  Entry(
             long offset,
-            int  position,
-            int  message
+            int  message,
+            int  position
             )
         {
             this.offset = offset;
-            this.position = position;
             this.message = message;
+            this.position = position;
         }
 
         public long offset()
--- src/test/java/org/reaktivity/nukleus/kafka/internal/cache/PartitionCacheTest.java
+++ src/test/java/org/reaktivity/nukleus/kafka/internal/cache/PartitionCacheTest.java
@@ -33,6 +33,8 @@ import org.reaktivity.nukleus.kafka.internal.stream.HeadersFW;
 
 public final class PartitionCacheTest
 {
+    private static final int TOMBSTONE_LIFETIME_MILLIS = 1;
+
     private MessageCache messageCache;
 
     private DirectBuffer key = asBuffer("key");
@@ -48,7 +50,7 @@ public final class PartitionCacheTest
         }
     };
 
-    private PartitionCache cache = new PartitionCache(5, messageCache);
+    private PartitionCache cache = new PartitionCache(5, TOMBSTONE_LIFETIME_MILLIS, messageCache);
 
     @Test
     public void shouldAddMessage()
@@ -64,9 +66,44 @@ public final class PartitionCacheTest
     }
 
     @Test
-    public void shouldReturnEmptyIteratorIfEmpty()
+    public void shouldAddTombstoneMessageAndReportUntilTombstoneExpires() throws Exception
     {
+        cache.add(0L, 2L, 123, 456, key, headers, null);
         Iterator<PartitionCache.Entry> iterator = cache.entries(0L);
+        assertTrue(iterator.hasNext());
+        Entry entry = iterator.next();
+        assertEquals(1L, entry.offset());
+        assertEquals(PartitionCache.TOMBSTONE_MESSAGE, entry.message());
+        Thread.sleep(TOMBSTONE_LIFETIME_MILLIS);
+        iterator = cache.entries(0L);
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void shouldNotAddMessagesWithOffsetsOutOfOrder()
+    {
+        context.checking(new Expectations()
+        {
+            {
+                oneOf(messageCache).put(124, 457, asBuffer("key2"), headers, value);
+                will(returnValue(0));
+                oneOf(messageCache).put(126, 459, asBuffer("key4"), headers, value);
+                will(returnValue(1));
+            }
+        });
+        cache.setPartitionStartOffset(100L);
+        cache.add(101L, 111L, 123, 456, asBuffer("key1"), headers, value);
+        cache.add(100L, 101L, 124, 457, asBuffer("key2"), headers, value);
+        cache.add(102L, 111L, 125, 458, asBuffer("key3"), headers, value);
+        cache.add(101L, 102L, 126, 459, asBuffer("key4"), headers, value);
+        Iterator<PartitionCache.Entry> iterator = cache.entries(100L);
+        assertTrue(iterator.hasNext());
+        Entry entry = iterator.next();
+        assertEquals(100L, entry.offset());
+        assertEquals(0, entry.message());
+        entry = iterator.next();
+        assertEquals(101L, entry.offset());
+        assertEquals(1, entry.message());
         assertFalse(iterator.hasNext());
     }
 
@@ -92,6 +129,77 @@ public final class PartitionCacheTest
         entry = iterator.next();
         assertEquals(1L, entry.offset());
         assertEquals(1, entry.message());
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void shouldIterateFromNextHighestOffsetWhenRequestedOffsetNotFound()
+    {
+        context.checking(new Expectations()
+        {
+            {
+                oneOf(messageCache).put(123, 456, asBuffer("key1"), headers, value);
+                will(returnValue(0));
+                oneOf(messageCache).put(124, 457, asBuffer("key2"), headers, value);
+                will(returnValue(1));
+                oneOf(messageCache).put(125, 458, asBuffer("key3"), headers, value);
+                will(returnValue(1));
+                oneOf(messageCache).replace(1, 126, 459, asBuffer("key2"), headers, value);
+                will(returnValue(1));
+            }
+        });
+        cache.add(0L, 1L, 123, 456, asBuffer("key1"), headers, value);
+        cache.add(0L, 2L, 124, 457, asBuffer("key2"), headers, value);
+        cache.add(0L, 3L, 125, 458, asBuffer("key3"), headers, value);
+        cache.add(0L, 11L, 126, 459, asBuffer("key2"), headers, value);
+        Iterator<PartitionCache.Entry> iterator = cache.entries(5L);
+        assertTrue(iterator.hasNext());
+        Entry entry = iterator.next();
+        assertEquals(10L, entry.offset());
+        assertEquals(1, entry.message());
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void shouldReturnIteratorWithRequestedOffsetAndNoMessageWhenCacheIsEmpty()
+    {
+        Iterator<PartitionCache.Entry> iterator = cache.entries(100L);
+        assertTrue(iterator.hasNext());
+        Entry entry = iterator.next();
+        assertEquals(100L, entry.offset());
+        assertEquals(MessageCache.NO_MESSAGE, entry.message());
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void shouldReturnIteratorWithRequestedOffsetAndNoMessageWhenRequestOffsetExceedsHighestOffsetSeenSoFar()
+    {
+        context.checking(new Expectations()
+        {
+            {
+                oneOf(messageCache).put(123, 456, asBuffer("key1"), headers, value);
+                will(returnValue(0));
+            }
+        });
+        cache.add(0L, 101L, 123, 456, asBuffer("key1"), headers, value);
+        Iterator<PartitionCache.Entry> iterator = cache.entries(102L);
+        assertTrue(iterator.hasNext());
+        Entry entry = iterator.next();
+        assertEquals(102L, entry.offset());
+        assertEquals(MessageCache.NO_MESSAGE, entry.message());
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void shouldCacheHistoricalMessageWhenItHasBeenEvicted()
+    {
+         throw new RuntimeException("Test not yet implemented");
+    }
+
+    @Test
+    public void shouldNotCacheHistoricalMessageWhenItHasNotBeenEvicted()
+    {
+         throw new RuntimeException("Test not yet implemented");
     }
 
     @Test
@@ -123,6 +231,24 @@ public final class PartitionCacheTest
         assertEquals(3L, entry.offset());
         assertEquals(0, entry.message());
         assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void shouldReportEntryAsString()
+    {
+        context.checking(new Expectations()
+        {
+            {
+                oneOf(messageCache).put(123, 456, asBuffer("key1"), headers, value);
+                will(returnValue(77));
+            }
+        });
+        cache.add(0L, 100L, 123, 456, asBuffer("key1"), headers, value);
+        Iterator<PartitionCache.Entry> iterator = cache.entries(0L);
+        Entry entry = iterator.next();
+        String result = entry.toString();
+        assertTrue(result.contains("99"));
+        assertTrue(result.contains("77"));
     }
 
     private static DirectBuffer asBuffer(String value)
