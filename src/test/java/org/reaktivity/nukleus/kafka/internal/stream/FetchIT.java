99d7c38025cf1c06ea65d42ae65de2c2d2b61f28
Chris Barrow <chris.barrow@kaazing.com>
2018-07-24 09:34:42 -0700

    Fixed a sporadic test failure due to timing

--- src/test/java/org/reaktivity/nukleus/kafka/internal/stream/FetchIT.java
+++ src/test/java/org/reaktivity/nukleus/kafka/internal/stream/FetchIT.java
@@ -198,6 +198,10 @@ public class FetchIT
     @ScriptProperty("networkAccept \"nukleus://target/streams/kafka\"")
     public void shouldReceiveMessageAndTombstoneWithMultivaluedHeaderUpdated() throws Exception
     {
+        k3po.start();
+        k3po.awaitBarrier("CLIENT_TWO_CONNECTED");
+        awaitWindowFromClient();
+        k3po.notifyBarrier("DELIVER_FIRST_FETCH_RESPONSE");
         k3po.finish();
     }
 
