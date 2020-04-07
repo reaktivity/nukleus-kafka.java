28b9839bf40379e6bfb1dc36f591468fb7f25c7d
John Fallows <john.fallows@kaazing.com>
2020-04-07 11:53:39 -0700

    Ensure LATEST offset can advance as needed

--- src/main/java/org/reaktivity/nukleus/kafka/internal/stream/KafkaCacheServerFetchFactory.java
+++ src/main/java/org/reaktivity/nukleus/kafka/internal/stream/KafkaCacheServerFetchFactory.java
@@ -1076,7 +1076,7 @@ public final class KafkaCacheServerFetchFactory implements StreamFactory
             this.replyId = supplyReplyId.applyAsLong(initialId);
             this.affinity = affinity;
             this.authorization = authorization;
-            this.partitionOffset = Math.min(partitionOffset, group.partitionOffset);
+            this.partitionOffset = partitionOffset;
         }
 
         private void onServerMessage(
